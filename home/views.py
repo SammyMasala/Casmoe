cee46b8f1546d8b522e6b51d4fd358ef640eef7b
ChickyMasala <samuel.goh96@gmail.com>
2024-01-04 22:57:45 +0800

    homepage

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template import loader
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie 
 def home_page(request):    
     context = {}
     return render(request, "home.html", context)
