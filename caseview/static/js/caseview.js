bef362d33471d496de883a25343c52035be5e8cd
ChickyMasala <samuel.goh96@gmail.com>
2024-10-14 10:47:27 +0800

    v1.1.0: fix case_id ref to case views

--- caseview/static/js/caseview.js
+++ caseview/static/js/caseview.js
@@ -13,3 +13,24 @@ function clearChild(elemId){
 
     return true;
 }
+
+function handleNavSummary(){
+    const path = window.location.pathname
+    const segments = path.split("/")
+    const caseId = segments.at(-1)
+    goToSummary(caseId)
+}
+
+function handleNavGraph(){
+    const path = window.location.pathname
+    const segments = path.split("/")
+    const caseId = segments.at(-1)
+    goToGraph(caseId)
+}
+
+function handleNavLine(){
+    const path = window.location.pathname
+    const segments = path.split("/")
+    const caseId = segments.at(-1)
+    goToLine(caseId)
+}
